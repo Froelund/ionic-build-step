9d3abb18c2417bb03dd0b55241af27c56888f9b2
Kristian Frølund Jørgensen <kristian@froelund.net>
2015-06-09 17:12:23 +0400

    Extracted env setup to _setup, and removed installation of fastlane

---
+++ _setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+command_exists () {
+	command -v "$1" >/dev/null 2>&1 ;
+}
+
+ensure_command () {
+	if command_exists $1 ; then
+		echo " [!] Already installed: $1"
+	else
+		echo " -> installing $1 with NPM"
+		npm -g install $1
+	fi
+}
+
+STARTTIME=$(date +%s)
+
+ensure_command cordova
+ensure_command ionic
+ensure_command bower
+ensure_command gulp
+
+ENDTIME=$(date +%s)
+echo
+echo " (i) Setup took $(($ENDTIME - $STARTTIME)) seconds to complete"
+echo
--- step.sh
+++ step.sh
@@ -2,7 +2,21 @@
 
 echo "Building ${PLATFORMS}!"
 
-gem install fastlane &&
-npm -g install cordova ionic bower gulp &&
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function fail_if_cmd_error {
+	local last_cmd_result=$?
+	local err_msg=$1
+	if [ ${last_cmd_result} -ne 0 ]; then
+		echo " [!] Error description: ${err_msg}"
+		echo "     (i) Exit code was: ${last_cmd_result}"
+		exit ${last_cmd_result}
+	fi
+}
+
+bash "${THIS_SCRIPT_DIR}/_setup.sh"
+fail_if_cmd_error "Could not install required tools"
+
+ionic platform rm $PLATFORMS &&
+ionic platform add $PLATFORMS &&
 ionic build $PLATFORMS --release
-exit 0
