63640ddf00cf905f4164ee7b5572f85fffced9c3
Kristian Frølund Jørgensen <kristian@froelund.net>
2015-06-09 16:37:15 +0400

    brew already install node. fixed name of template

--- step.sh
+++ step.sh
@@ -2,11 +2,8 @@
 
 echo "Building ${PLATFORMS}!"
 
-brew update &&
-brew install node &&
 gem install fastlane &&
-npm -g install cordova ionic bower gulp 
-
+npm -g install cordova ionic bower gulp &&
 ionic install $PLATFORMS --release
 
 exit 0
